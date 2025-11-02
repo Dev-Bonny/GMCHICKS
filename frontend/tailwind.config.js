d6a8f4170d074eca920f1dc4467e90ed21c6fbf1
Dev-Bonny <kangangibonface2021@gmail.com>
2025-11-02 20:46:39 +0300

    fixed css issue probably

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,25 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         primary: {
-          50: '#ff2626',
-          100: '#ffdefc',
-          200: '#ff0290',
-          300: '#ff0457',
-          400: '#ffca54',
-          500: '#ffbf30',
-          600: '#ffb0a9',
-          700: '#ffb0a7',
-          800: '#ffaa82',
-          900: '#ee9916',
-        },
-      },
+          50: '#f5f7ff',
+          500: '#3b82f6',
+          600: '#2563eb',
+        }
+      }
     },
   },
   plugins: [],
